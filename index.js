d37ad5fd1bdd5649d33e4d95aac645b3d7a8f281
Peter Smaznoy <ithespv@gmail.com>
2019-07-07 20:17:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const showTips = () => [].slice.call(document.querySelectorAll('.reply_ticket')).forEach(item => { 
-  if (item.style['border-color'] !== '') {
-    item.style.display = 'block'
-  }
-})
+(() => {
+  const isTraining = (() => {
+    const trainingProp = location.search.split('&').find(item => item.includes('training'))
+    return trainingProp ? trainingProp.split('=')[1] === 'on' : false
+  })()
+  
+  const showTips = () => [].slice.call(document.querySelectorAll('.reply_ticket')).forEach(item => { 
+    if (item.style['border-color'] !== '' && isTraining) {
+      item.style.display = 'block'
+    }
+  })
+  
+  ;[].slice.call(document.querySelectorAll('.label_raio')).forEach(label => label.addEventListener('click', showTips))
 
-;[].slice.call(document.querySelectorAll('.label_raio')).forEach(label => label.addEventListener('click', showTips))
+  const element = document.querySelector('.subscriptions_not')
+  if (element) {
+    element.classList.remove('subscriptions_not')
+  }
+})()
