d4f373925fe03b6d9eadb6a4efb0bf9d5b35a379
spasea <ithespv@gmail.com>
2023-10-17 22:52:01 +0300

    Add imapge uploading

--- app.ts
+++ app.ts
@@ -1,26 +1,41 @@
-// import { JSDOM } from 'jsdom';
-// import HTMLNodes from './src/HTMLNodes';
-// import VodiyLoader from './src/VodiyLoader';
-// import Storage from './src/Storage';
-//
-// const main = async () => {
-//   const instance = new VodiyLoader();
-//   //
-//   await instance.fillUpQuestions();
-//   //
-//   const [questions, themes, tickets] = await Promise.all([
-//     instance.getQuestions(),
-//     instance.getThemes(),
-//     instance.getTickets(),
-//   ]);
-//
-//   Storage.saveJSONToStorage('questions.json', questions);
-//   Storage.saveJSONToStorage('themes.json', themes);
-//   Storage.saveJSONToStorage('tickets.json', tickets);
-//
-//   // console.log(new URLProcessor('https://vodiy.ua').toAbsolute('/media'));
-//
-//   // Cloudinary.v2.uploader.upload('');
-// };
-//
-// main();
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+dotenv.config();
+
+const main = async () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    private_cdn: false,
+    secure_distribution: null,
+    secure: true,
+  });
+
+  // const instance = new VodiyLoader();
+  // //
+  // await instance.fillUpQuestions();
+  // //
+  // const [questions, themes, tickets] = await Promise.all([
+  //   instance.getQuestions(),
+  //   instance.getThemes(),
+  //   instance.getTickets(),
+  // ]);
+  //
+  // Storage.saveJSONToStorage('questions.json', questions);
+  // Storage.saveJSONToStorage('themes.json', themes);
+  // Storage.saveJSONToStorage('tickets.json', tickets);
+
+  // console.log(new URLProcessor('https://vodiy.ua').toAbsolute('/media'));
+
+  // Cloudinary.v2.uploader.upload('');
+
+  console.log('start');
+
+  // await Storage.uploadMedia('https://vodiy.ua/media/questions/1219_6.jpg');
+
+  console.log('done');
+};
+
+main();
--- src/Storage.ts
+++ src/Storage.ts
@@ -1,9 +1,42 @@
 import fs from 'fs';
 import path from 'path';
+import { JSDOM } from 'jsdom';
+import { v2 as cloudinary } from 'cloudinary';
 
 class Storage {
   static saveJSONToStorage(filename, content) {
     fs.writeFileSync(path.resolve(__dirname, '../', filename), JSON.stringify(content));
+  }
+
+  static saveMedia(filename, content) {
+    const mediaDir = path.resolve(__dirname, '../media');
+    if (!fs.existsSync(mediaDir)) {
+      fs.mkdirSync(mediaDir);
+    }
+    fs.writeFileSync(path.resolve(__dirname, '../media', filename), content);
+  }
+
+  static async uploadMedia(path) {
+    const { CURRENT_YEAR } = process.env;
+    const jsdomInstance = new JSDOM('');
+    const url = new jsdomInstance.window.URL(path);
+    const [, media, type] = url.pathname.split('/').slice(0, -1);
+    const targetFolder = ['', media, CURRENT_YEAR, type].join('/');
+
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+      folder: targetFolder,
+    };
+
+    try {
+      // Upload the image
+      const result = await cloudinary.uploader.upload(path, options);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
